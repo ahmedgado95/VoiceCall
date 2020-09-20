fa9c71c7b6eb189a61ed518874136e9e924a6481
Ahmed Gado <ahmedgado2001@gmail.com>
2020-09-20 12:12:10 +0200

    update Code

--- TestVoiceCall/AppDelegate.swift
+++ TestVoiceCall/AppDelegate.swift
@@ -7,31 +7,26 @@
 //
 
 import UIKit
+import JitsiMeet
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+    var window: UIWindow?
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        guard let launchOptions = launchOptions else { return false }
+        return JitsiMeet.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
     }
 
-    // MARK: UISceneSession Lifecycle
+    // MARK: - Linking delegate methods
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    func application(_ application: UIApplication,
+                     continue userActivity: NSUserActivity,
+                     restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        return JitsiMeet.sharedInstance().application(application, continue: userActivity, restorationHandler: restorationHandler)
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return JitsiMeet.sharedInstance().application(app, open: url, options: options)
     }
-
-
 }
-
--- TestVoiceCall/ViewController.swift
+++ TestVoiceCall/ViewController.swift
@@ -7,14 +7,53 @@
 //
 
 import UIKit
+import JitsiMeet
 
 class ViewController: UIViewController {
+
+    @IBOutlet weak var callButton: UIButton!
+    
+    fileprivate var jitsiMeetView: JitsiMeetView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    fileprivate func cleanUp() {
+        if(jitsiMeetView != nil) {
+            dismiss(animated: true, completion: nil)
+            jitsiMeetView = nil
+        }
+    }
+    @IBAction func callButtonPressed(_ sender: Any) {
+        let room: String = "Gado"
+              if(room.count < 1) {
+                  return
+              }
+              
+              // create and configure jitsimeet view
+              let jitsiMeetView = JitsiMeetView()
+              jitsiMeetView.delegate = self
+              self.jitsiMeetView = jitsiMeetView
+              let options = JitsiMeetConferenceOptions.fromBuilder { (builder) in
+                  builder.welcomePageEnabled = false
+                  builder.room = room
+              }
+                      
+              // setup view controller
+              let vc = UIViewController()
+              vc.modalPresentationStyle = .fullScreen
+              vc.view = jitsiMeetView
+              
+              // join room and display jitsi-call
+              jitsiMeetView.join(options)
+              present(vc, animated: true, completion: nil)
+    }
+    
 }
 
+extension ViewController: JitsiMeetViewDelegate {
+    func conferenceTerminated(_ data: [AnyHashable : Any]!) {
+        cleanUp()
+    }
+}
